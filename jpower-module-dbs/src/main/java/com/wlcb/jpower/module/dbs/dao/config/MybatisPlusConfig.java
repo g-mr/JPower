370e50701d68a5453ac4fc048685a16bfda6f13f
郭丁志 <ding931226@yeah.net>
2020-07-07 18:08:32 +0800

    切换mybatisplus版本

--- jpower-module-dbs/src/main/java/com/wlcb/jpower/module/dbs/dao/config/MybatisPlusConfig.java
+++ jpower-module-dbs/src/main/java/com/wlcb/jpower/module/dbs/dao/config/MybatisPlusConfig.java
@@ -2,9 +2,11 @@ package com.wlcb.jpower.module.dbs.dao.config;
 
 import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @ClassName MybatisPlusConfig
@@ -13,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
  * @Date 2020-07-03 11:47
  * @Version 1.0
  */
+@EnableTransactionManagement
 @Configuration
 public class MybatisPlusConfig {
 
@@ -34,8 +37,15 @@ public class MybatisPlusConfig {
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
-        paginationInterceptor.setDialectType("mysql");
-        return new PaginationInterceptor();
+        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
+        // paginationInterceptor.setOverflow(false);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        // paginationInterceptor.setLimit(500);
+        // 开启 count 的 join 优化,只针对部分 left join
+        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
+        return paginationInterceptor;
+//        paginationInterceptor.setDialectType("mysql");
+//        return new PaginationInterceptor();
     }
 
 }
