65f0ab07a9faac4f211df14b772b8a31b909c9c1
guodingzhi <ding931226@yeah.net>
2020-08-23 18:19:49 +0800

    新增网关

---
+++ jpower-module-gataway/src/main/java/com/wlcb/jpower/GetwayApplication.java
@@ -0,0 +1,18 @@
+package com.wlcb.jpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName GetwayApplication
+ * @Description TODO
+ * @Author 郭丁志
+ * @Date 2020/8/23 0023 17:49
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class GetwayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GetwayApplication.class, args);
+    }
+}
