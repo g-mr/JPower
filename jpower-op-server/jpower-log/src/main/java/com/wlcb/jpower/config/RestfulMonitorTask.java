511d41f378b7cd54bb7ab7ac89eb252638d5b741
guodingzhi <ding931226@yeah.net>
2021-04-02 00:55:36 +0800

    实现接口监控的定时任务调度器

---
+++ jpower-op-server/jpower-log/src/main/java/com/wlcb/jpower/config/RestfulMonitorTask.java
@@ -0,0 +1,48 @@
+package com.wlcb.jpower.config;
+
+import com.wlcb.jpower.module.common.utils.DateUtil;
+import com.wlcb.jpower.module.common.utils.Fc;
+import com.wlcb.jpower.properties.MonitorRestfulProperties;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.scheduling.support.CronTrigger;
+
+/**
+ * @Description TODO 接口监控定时任务执行器
+ * @Author mr.g
+ * @Date 2021/4/1 0001 22:26
+ */
+@Configuration
+@EnableScheduling
+@EnableConfigurationProperties(MonitorRestfulProperties.class)
+@AllArgsConstructor
+public class RestfulMonitorTask implements SchedulingConfigurer {
+
+    private final MonitorRestfulProperties properties;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        if (properties.getEnable()){
+            properties.getRoutes().forEach(route ->
+                scheduledTaskRegistrar.addTriggerTask(() -> process(route),
+                        triggerContext -> {
+                            String cron = properties.getCron();
+                            if (Fc.isBlank(cron)) {
+                                throw new RuntimeException("restfulMonitorTask cron is null");
+                            }
+                            return new CronTrigger(cron).nextExecutionTime(triggerContext);
+                        })
+            );
+        }
+    }
+
+    private void process(MonitorRestfulProperties.Routes route) {
+
+        System.out.println(route.getName() + ":" + DateUtil.getDateTime());
+    }
+
+}
---
+++ jpower-op-server/jpower-log/src/main/java/com/wlcb/jpower/properties/MonitorRestfulProperties.java
@@ -0,0 +1,45 @@
+package com.wlcb.jpower.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName MonitorRestfulProperties
+ * @Author mr.g
+ */
+@Data
+@ConfigurationProperties(prefix = "jpower.monitor-restful")
+public class MonitorRestfulProperties {
+
+    /**
+     * 是否启用接口监控
+     */
+    private Boolean enable = false;
+    /**
+     * 监控时间，cron表达式
+     */
+    private String cron = "0 0 1 * * ?";
+    /**
+     * 监控项目列表
+     */
+    private List<Routes> routes = new ArrayList<>();
+
+    @Data
+    public static class Routes{
+        /**
+         * 项目名称
+         */
+        private String name;
+        /**
+         * 项目地址
+         */
+        private String url;
+        /**
+         * 获取接口信息地址
+         */
+        private String location;
+    }
+}
