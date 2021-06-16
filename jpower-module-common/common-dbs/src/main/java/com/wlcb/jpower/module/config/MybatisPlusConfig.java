1c71b9852b3134efdfb79e37232807fa2fa1fb9a
郭丁志 <ding931226@yeah.net>
2021-06-16 13:23:20 +0800

    加载mybatis配置方式修改

--- jpower-module-common/common-dbs/src/main/java/com/wlcb/jpower/module/config/MybatisPlusConfig.java
+++ jpower-module-common/common-dbs/src/main/java/com/wlcb/jpower/module/config/MybatisPlusConfig.java
@@ -7,7 +7,6 @@ import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
 import com.wlcb.jpower.module.common.deploy.property.YamlAndPropertySourceFactory;
-import com.wlcb.jpower.module.common.support.EnvBeanUtil;
 import com.wlcb.jpower.module.config.interceptor.DemoInterceptor;
 import com.wlcb.jpower.module.config.interceptor.MybatisSqlPrintInterceptor;
 import com.wlcb.jpower.module.config.properties.DemoProperties;
@@ -47,9 +46,6 @@ public class MybatisPlusConfig {
      **/
     @Bean
     public GlobalConfig globalConfig() {
-
-        System.out.println(EnvBeanUtil.getString("env.datasource"));
-
         GlobalConfig globalConfig = new GlobalConfig();
         globalConfig.setMetaObjectHandler(new UpdateRelatedFieldsMetaHandler());
         return globalConfig;
