6edc5d61a0d0d6c4cddd12a5212a14e37e909603
郭丁志 <ding931226@yeah.net>
2022-01-25 15:15:48 +0800

    鉴权filter恢复

--- jpower-module-gateway/src/main/java/com/wlcb/jpower/gateway/config/AuthFilter.java
+++ jpower-module-gateway/src/main/java/com/wlcb/jpower/gateway/config/AuthFilter.java
@@ -3,19 +3,26 @@ package com.wlcb.jpower.gateway.config;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wlcb.jpower.gateway.utils.ExculdesUrl;
+import com.wlcb.jpower.gateway.utils.IpUtil;
+import com.wlcb.jpower.gateway.utils.TokenUtil;
 import com.wlcb.jpower.module.common.auth.RoleConstant;
 import com.wlcb.jpower.module.common.cache.CacheNames;
 import com.wlcb.jpower.module.common.redis.RedisUtil;
 import com.wlcb.jpower.module.common.support.ChainMap;
 import com.wlcb.jpower.module.common.utils.Fc;
+import com.wlcb.jpower.module.common.utils.JwtUtil;
+import com.wlcb.jpower.module.common.utils.constants.StringPool;
 import com.wlcb.jpower.module.common.utils.constants.TokenConstant;
 import com.wlcb.jpower.module.properties.AuthProperties;
+import io.jsonwebtoken.Claims;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.core.Ordered;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
@@ -54,7 +61,46 @@ public class AuthFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return chain.filter(exchange);
+        Route route = (Route) exchange.getAttributes().get(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
+        String currentPath = exchange.getRequest().getURI().getPath();
+        if (currentPath.startsWith("/"+route.getId())){
+            currentPath = currentPath.replace("/"+route.getId(),"");
+        }
+
+        //不鉴权得URL
+        if (isSkip(currentPath)){
+            return chain.filter(exchange);
+        }
+
+        String token = TokenUtil.getToken(exchange.getRequest());
+        if (Fc.isNotBlank(token)) {
+
+            Claims claims = JwtUtil.parseJWT(token);
+            if (!redisUtil.exists(CacheNames.TOKEN_URL_KEY + token)){
+                return proxyAuthenticationRequired(exchange.getResponse(), "令牌已过期，请重新登陆");
+            }
+            Object o = redisUtil.get(CacheNames.TOKEN_URL_KEY + token);
+            List<String> listUrl = Fc.isNull(o)?new ArrayList<>():(List<String>) o;
+            if (Fc.isNull(claims) || !Fc.contains(listUrl, currentPath)) {
+                return unAuth(exchange.getResponse(), "请求未授权");
+            }
+
+            Object dataAuth = redisUtil.get(CacheNames.TOKEN_DATA_SCOPE_KEY + token);
+            Map<String,String> map = Fc.isNull(dataAuth) ? ChainMap.newMap() : (Map<String, String>) dataAuth;
+            return chain.filter(addHeader(exchange, StringPool.EMPTY, map.get(currentPath)));
+        }else {
+            //白名单
+            String ip = IpUtil.getIP(exchange.getRequest());
+            if (Fc.contains(authProperties.getWhileIp(),ip)){
+                return chain.filter(addHeader(exchange,ip,StringPool.EMPTY));
+            }
+
+            //匿名用户
+            if (getIsAnonymous(currentPath)){
+                return chain.filter(addHeader(exchange,"anonymous",StringPool.EMPTY));
+            }
+            return proxyAuthenticationRequired(exchange.getResponse(), "缺失令牌，鉴权失败");
+        }
     }
 
     private boolean isSkip(String path) {
