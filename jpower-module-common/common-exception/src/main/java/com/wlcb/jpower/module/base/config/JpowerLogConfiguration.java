cbec4ec8f3b48a56dc1db0295eb34a22d7fc22f7
mr.g <ding931226@yeah.net>
2022-05-30 00:58:25 +0800

    feign报错日志优化

--- jpower-module-common/common-exception/src/main/java/com/wlcb/jpower/module/base/config/JpowerLogConfiguration.java
+++ jpower-module-common/common-exception/src/main/java/com/wlcb/jpower/module/base/config/JpowerLogConfiguration.java
@@ -10,8 +10,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplicat
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.Resource;
-
 /**
  * 日志工具配置
  *
@@ -22,9 +20,6 @@ import javax.annotation.Resource;
 @ConditionalOnWebApplication
 public class JpowerLogConfiguration {
 
-    @Resource
-    private LogClient logService;
-
     @Bean
     public OperateLogAspect apiLogAspect() {
         return new OperateLogAspect();
@@ -32,13 +27,13 @@ public class JpowerLogConfiguration {
 
     @Bean
     @ConditionalOnMissingBean(name = "operateLogListener")
-    public OperateLogListener operateLogListener(JpowerProperties jpowerProperties) {
+    public OperateLogListener operateLogListener(LogClient logService, JpowerProperties jpowerProperties) {
         return new OperateLogListener(logService, jpowerProperties);
     }
 
     @Bean
     @ConditionalOnMissingBean(name = "errorLogListener")
-    public ErrorLogListener errorLogListener(JpowerProperties jpowerProperties) {
+    public ErrorLogListener errorLogListener(LogClient logService, JpowerProperties jpowerProperties) {
         return new ErrorLogListener(logService, jpowerProperties);
     }
 }
--- jpower-module-common/common-exception/src/main/java/com/wlcb/jpower/module/base/feign/LogClient.java
+++ jpower-module-common/common-exception/src/main/java/com/wlcb/jpower/module/base/feign/LogClient.java
@@ -3,26 +3,40 @@ package com.wlcb.jpower.module.base.feign;
 import com.wlcb.jpower.module.base.model.ErrorLogDto;
 import com.wlcb.jpower.module.base.model.OperateLogDto;
 import com.wlcb.jpower.module.base.vo.ResponseData;
+import com.wlcb.jpower.module.common.utils.Fc;
 import com.wlcb.jpower.module.common.utils.constants.AppConstant;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Feign接口类
  *
  * @author mr.g
  */
-@FeignClient(value = AppConstant.JPOWER_LOG, fallback = LogClientFallback.class, path = "/log")
-public interface LogClient {
+@Slf4j
+@Service
+@AllArgsConstructor
+public class LogClient {
+
+	private RestTemplate restTemplate;
 
 	/**
 	 * 保存操作日志
-	 * @param log
+	 * @param operateLog
 	 * @return
 	 */
-	@PostMapping("/saveOperateLog")
-	ResponseData<Boolean> saveOperateLog(@RequestBody OperateLogDto log);
+	public void saveOperateLog(OperateLogDto operateLog){
+		try {
+			ResponseData responseData = restTemplate.postForObject("http://"+ AppConstant.JPOWER_LOG+"/log/saveOperateLog",operateLog,ResponseData.class);
+			if (Fc.isNull(responseData) || !responseData.isStatus()){
+				log.error("操作日志保存失败={}",responseData);
+			}
+		} catch (Exception e){
+			log.error("操作日志保存失败={}", e.getMessage());
+		}
+	}
 
 	/**
 	 * 保存错误日志
@@ -30,6 +44,22 @@ public interface LogClient {
 	 * @param errorLog
 	 * @return com.wlcb.jpower.module.base.vo.ResponseData<java.lang.Boolean>
 	 */
-	@PostMapping("/saveErrorLog")
-	ResponseData<Boolean> saveErrorLog(@RequestBody ErrorLogDto errorLog);
+	public void saveErrorLog(ErrorLogDto errorLog){
+		if (Fc.equalsValue(errorLog.getServerName(),AppConstant.JPOWER_LOG) &&
+			Fc.equalsValue(errorLog.getMethodClass(),"com.wlcb.jpower.feign.LogClientController") &&
+			Fc.equalsValue(errorLog.getMethodName(),"saveErrorLog") &&
+			Fc.equalsValue(errorLog.getUrl(),"/log/saveErrorLog")){
+			log.error("保存错误日志接口错误={}",errorLog);
+			return;
+		}
+
+		try{
+			ResponseData responseData = restTemplate.postForObject("http://"+ AppConstant.JPOWER_LOG+"/log/saveErrorLog",errorLog,ResponseData.class);
+			if (Fc.isNull(responseData) || !responseData.isStatus()){
+				log.error("操作日志保存失败={}",responseData);
+			}
+		}catch (Exception e){
+			log.error("操作日志保存失败={}", e.getMessage());
+		}
+	}
 }
