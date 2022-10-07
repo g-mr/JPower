3401da477bcb21219ef6238b627167d46db779ec
mr.g <ding931226@yeah.net>
2022-10-07 15:12:17 +0800

    @Function注解功能点拦截

--- jpower-module-common/common-tool/src/main/java/com/wlcb/jpower/module/common/tarnsaction/TransactionAdviceConfig.java
+++ jpower-module-common/common-tool/src/main/java/com/wlcb/jpower/module/common/tarnsaction/TransactionAdviceConfig.java
@@ -1,10 +1,10 @@
 package com.wlcb.jpower.module.common.tarnsaction;
 
+import lombok.AllArgsConstructor;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.aop.Advisor;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -22,10 +22,10 @@ import org.springframework.transaction.interceptor.TransactionInterceptor;
  */
 @Aspect
 @Configuration
+@AllArgsConstructor
 public class TransactionAdviceConfig {
     private static final String AOP_POINTCUT_EXPRESSION = "execution (* com.wlcb..*.service..*.*(..))";
 
-    @Autowired
     private PlatformTransactionManager transactionManager;
 
     @Bean
