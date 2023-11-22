b46125df4ab2bdaf4229b86933ba7481f4a5b05c
administrator <yanf@qq.com>
2023-11-22 12:12:39 +0800

    时间处理

--- jpower-module-common/common-utils/src/main/java/com/wlcb/jpower/module/common/jackson/JpowerJavaTimeModule.java
+++ jpower-module-common/common-utils/src/main/java/com/wlcb/jpower/module/common/jackson/JpowerJavaTimeModule.java
@@ -1,20 +1,29 @@
 package com.wlcb.jpower.module.common.jackson;
 
 import cn.hutool.core.date.DatePattern;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.datatype.jsr310.PackageVersion;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 /**
  * java 8 时间默认序列化
@@ -22,22 +31,59 @@ import java.time.format.DateTimeFormatter;
  * @author mr.g
  **/
 @Configuration
-public class JpowerJavaTimeModule extends SimpleModule {
+public class JpowerJavaTimeModule{
 
-    public JpowerJavaTimeModule() {
-        super(PackageVersion.VERSION);
-        this.addSerializer(LocalDateTime.class,
+    @Bean
+    @ConditionalOnMissingBean(DateJacksonConverter.class)
+    public DateJacksonConverter dateJacksonConverter() {
+        return new DateJacksonConverter();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(Jackson2ObjectMapperFactoryBean.class)
+    public Jackson2ObjectMapperFactoryBean jackson2ObjectMapperFactoryBean(@Autowired DateJacksonConverter dateJacksonConverter) {
+        Jackson2ObjectMapperFactoryBean jackson2ObjectMapperFactoryBean = new Jackson2ObjectMapperFactoryBean();
+
+        jackson2ObjectMapperFactoryBean.setDeserializers(dateJacksonConverter);
+        return jackson2ObjectMapperFactoryBean;
+    }
+
+
+    @Bean
+    @ConditionalOnMissingBean(MappingJackson2HttpMessageConverter.class)
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter(@Autowired ObjectMapper objectMapper) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+
+        // 忽略json字符串中不识别的属性
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        // 忽略无法转换的对象
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        // PrettyPrinter 格式化输出
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        // NULL不参与序列化
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        // 指定时区
+        objectMapper.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
+
+        // java8日期日期处理
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class,
                 new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DatePattern.NORM_DATETIME_PATTERN)));
-        this.addSerializer(LocalDate.class,
+        javaTimeModule.addSerializer(LocalDate.class,
                 new LocalDateSerializer(DateTimeFormatter.ofPattern(DatePattern.NORM_DATE_PATTERN)));
-        this.addSerializer(LocalTime.class,
+        javaTimeModule.addSerializer(LocalTime.class,
                 new LocalTimeSerializer(DateTimeFormatter.ofPattern(DatePattern.NORM_TIME_PATTERN)));
-        this.addDeserializer(LocalDateTime.class,
+        javaTimeModule.addDeserializer(LocalDateTime.class,
                 new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DatePattern.NORM_DATETIME_PATTERN)));
-        this.addDeserializer(LocalDate.class,
+        javaTimeModule.addDeserializer(LocalDate.class,
                 new LocalDateDeserializer(DateTimeFormatter.ofPattern(DatePattern.NORM_DATE_PATTERN)));
-        this.addDeserializer(LocalTime.class,
+        javaTimeModule.addDeserializer(LocalTime.class,
                 new LocalTimeDeserializer(DateTimeFormatter.ofPattern(DatePattern.NORM_TIME_PATTERN)));
+        objectMapper.registerModule(javaTimeModule);
+
+        converter.setObjectMapper(objectMapper);
+        return converter;
     }
 
 }
