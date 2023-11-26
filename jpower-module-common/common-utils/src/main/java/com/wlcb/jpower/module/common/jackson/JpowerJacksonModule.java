4650c70866c7ce4903286e2cf673aec6196814bc
mr.g <ding931226@yeah.net>
2023-11-27 00:41:03 +0800

    雪花ID之后的一些问题

--- jpower-module-common/common-utils/src/main/java/com/wlcb/jpower/module/common/jackson/JpowerJacksonModule.java
+++ jpower-module-common/common-utils/src/main/java/com/wlcb/jpower/module/common/jackson/JpowerJacksonModule.java
@@ -31,7 +31,7 @@ import java.util.Date;
  * @author mr.g
  **/
 @Configuration
-public class JpowerJavaTimeModule{
+public class JpowerJacksonModule{
 
     @Bean
     @ConditionalOnMissingBean(DateJacksonConverter.class)
