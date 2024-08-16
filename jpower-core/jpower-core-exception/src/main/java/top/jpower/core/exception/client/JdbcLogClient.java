26c8c49f829da6bcb34835e27046d73b2ee06e6b
mr.g <ding931226@yeah.net>
2024-08-16 10:24:23 +0800

    异常模块优化

--- jpower-core/jpower-core-exception/src/main/java/top/jpower/core/exception/client/JdbcLogClient.java
+++ jpower-core/jpower-core-exception/src/main/java/top/jpower/core/exception/client/JdbcLogClient.java
@@ -1,6 +1,7 @@
 package top.jpower.core.exception.client;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import top.jpower.core.exception.model.ErrorLogDto;
@@ -20,17 +21,24 @@ public class JdbcLogClient implements LogClient {
     private final JdbcTemplate jdbcTemplate;
 
     /**
+     * 保存操作日志SQL
+     **/
+    private static final String OPERATE_LOG_SQL = "insert into tb_log_operate(id,server_name,server_ip,server_host,env,url,method,method_class,method_name,param,oper_ip,oper_name,oper_user_type,client_code,title,business_type,return_content,status,error_msg,record_id,content) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    /**
+     * 保存错误日志SQL
+     **/
+    private static final String ERROR_LOG_SQL = "insert into tb_log_error(id,server_name,server_ip,server_host,env,url,method,method_class,method_name,param,oper_ip,oper_name,oper_user_type,client_code,error,line_number,exception_name,message) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+    /**
      * 保存操作日志
      *
      * @param operateLog 操作日志
      * @return
      */
     @Override
+    @SneakyThrows(Exception.class)
     public void saveOperateLog(OperateLogDto operateLog) {
-        int count = jdbcTemplate.update("insert into tb_log_operate" +
-                        "(id,server_name,server_ip,server_host,env,url,method,method_class,method_name,param,oper_ip,oper_name,oper_user_type,client_code,title,business_type,return_content,status,error_msg,record_id,content) " +
-                        "values " +
-                        "(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+        int count = jdbcTemplate.update(OPERATE_LOG_SQL,
                 Fc.randomSnowFlakeId(),operateLog.getServerName(),operateLog.getServerIp(),operateLog.getServerHost(),operateLog.getEnv(),operateLog.getUrl(),operateLog.getMethod(),operateLog.getMethodClass(),operateLog.getMethodName(),operateLog.getParam(),operateLog.getOperIp(),operateLog.getOperName(),operateLog.getOperUserType(),operateLog.getClientCode(),operateLog.getTitle(),operateLog.getBusinessType(),operateLog.getReturnContent(),operateLog.getStatus(),operateLog.getErrorMsg(),operateLog.getRecordId(),operateLog.getContent());
         if (count <= 0){
             log.error("操作日志保存失败={}",count);
@@ -44,11 +52,9 @@ public class JdbcLogClient implements LogClient {
      * @author mr.g
      */
     @Override
+    @SneakyThrows(Exception.class)
     public void saveErrorLog(ErrorLogDto errorLog) {
-        int count = jdbcTemplate.update("insert into tb_log_error" +
-                        "(id,server_name,server_ip,server_host,env,url,method,method_class,method_name,param,oper_ip,oper_name,oper_user_type,client_code,error,line_number,exception_name,message) " +
-                        "values " +
-                        "(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+        int count = jdbcTemplate.update(ERROR_LOG_SQL,
                 Fc.randomSnowFlakeId(),errorLog.getServerName(),errorLog.getServerIp(),errorLog.getServerHost(),errorLog.getEnv(),errorLog.getUrl(),errorLog.getMethod(),errorLog.getMethodClass(),errorLog.getMethodName(),errorLog.getParam(),errorLog.getOperIp(),errorLog.getOperName(),errorLog.getOperUserType(),errorLog.getClientCode(),errorLog.getError(),errorLog.getLineNumber(),errorLog.getExceptionName(),errorLog.getMessage());
         if (count <= 0){
             log.error("错误日志保存失败={}",count);
