860d1024eb181078b97a274c5f8743fdad2a1677
mr.g <ding931226@yeah.net>
2025-06-28 00:36:45 +0800

    WebClient配置

--- jpower-gateway/src/main/java/top/jpower/jpower/gateway/config/WebClientConfig.java
+++ jpower-gateway/src/main/java/top/jpower/jpower/gateway/config/WebClientConfig.java
@@ -3,43 +3,39 @@ package top.jpower.jpower.gateway.config;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.client.loadbalancer.reactive.RetryableLoadBalancerExchangeFilterFunction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
+/**
+ * WebClient配置
+ * 负载均衡设置
+ *
+ * @author mr.g
+ **/
 @Configuration(proxyBeanMethods = false)
 public class WebClientConfig {
 
 
     @Bean
-    @LoadBalanced
-    public WebClient webClient(WebClient.Builder builder, RetryableLoadBalancerExchangeFilterFunction filter){
-        return builder
-//                .filter(filter)
-                .build();
+    public WebClient webClient(WebClient.Builder builder){
+        return builder.build();
     }
 
     @Bean
-    public ExchangeFilterFunction filterFunction(){
-        return (request, next) -> {
+    public WebClientCustomizer webClientCustomizer(){
+        return builder -> builder.filter((request, next) -> {
             System.out.println("进入到WebClient拦截器................");
             return next.exchange(request);
-        };
+        });
     }
 
     @Bean
-    public WebClientCustomizer webClientCustomizer(ObjectProvider<ExchangeFilterFunction> objectProvider){
-        return builder -> {
-            builder.filters(filters ->
-                    objectProvider.orderedStream()
-                            // todo DeferringLoadBalancerExchangeFilterFunction和RetryableLoadBalancerExchangeFilterFunction不能共存,不知道为什么
-                            // todo 需要查一下原因
-                            //.filter(filterFunction -> !(filterFunction instanceof DeferringLoadBalancerExchangeFilterFunction))
-                            .filter(filterFunction -> !(filterFunction instanceof RetryableLoadBalancerExchangeFilterFunction))
-                            .forEach(filters::add));
-        };
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder(ObjectProvider<WebClientCustomizer> customizerProvider) {
+        WebClient.Builder builder = WebClient.builder();
+        customizerProvider.orderedStream().forEach((customizer) -> customizer.customize(builder));
+        return builder;
     }
 
 }
