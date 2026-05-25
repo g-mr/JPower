f844038f9385c829b0222c4f6946883b7740a43e
mr.g <ding931226@yeah.net>
2026-05-26 00:27:22 +0800

    fix(dbs): 优化多租户处理逻辑与日志打印
    - 添加日志记录租户过滤忽略情况
    - 打印当前租户编码以便调试跟踪
    - 租户编码为空时不进行租户过滤
    - 通过工具类判断租户编码是否为空
    - 引入Slf4j日志功能和辅助工具类Fc

--- jpower-core/jpower-core-dbs/src/main/java/top/jpower/core/dbs/tenant/JpowerTenantHandler.java
+++ jpower-core/jpower-core-dbs/src/main/java/top/jpower/core/dbs/tenant/JpowerTenantHandler.java
@@ -3,6 +3,8 @@ package top.jpower.core.dbs.tenant;
 import com.mybatisflex.core.tenant.TenantFactory;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import top.jpower.core.util.utils.Fc;
 
 /**
  * 多租户处理
@@ -10,6 +12,7 @@ import lombok.Data;
  * @author mr.g
  */
 @Data
+@Slf4j
 @AllArgsConstructor
 public class JpowerTenantHandler implements TenantFactory {
 
@@ -24,9 +27,16 @@ public class JpowerTenantHandler implements TenantFactory {
     @Override
     public Object[] getTenantIds(String tableName) {
         if (properties.getExcludeTables().contains(tableName) || TenantContextHolder.getTenantSkip()){
+            log.debug("忽略租户过滤");
             return null;
         }
-        return new Object[]{TenantContextHolder.getTenantCode()};
+        String tenantCode = TenantContextHolder.getTenantCode();
+        log.debug("当前租户为 >> {}", tenantCode);
+        if (Fc.isBlank(tenantCode)) {
+            // 租户为空代表不进行租户过滤
+            return null;
+        }
+        return new Object[]{tenantCode};
     }
 
 }
